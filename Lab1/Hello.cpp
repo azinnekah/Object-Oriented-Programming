71c8c2f36e51fb9c62ec4185c21976149037dc27
azinnekah <azinnekah@cpp.edu>
2026-08-27 23:07:44 -0700

    Added Lab1 Folder, Containing Hello.cpp

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,15 @@
+/***
+ *  Adam Zinnekah -- ECE 2310 Object Oriented Programming Assignment 1- Setup - Hello World --- 8/27/2026
+ ***/
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+
+    return 0;
+}
